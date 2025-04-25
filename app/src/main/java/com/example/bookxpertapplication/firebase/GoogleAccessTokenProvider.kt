73b26ca99fe7f453669f69f7a4addbc287f342d5
Prajwal Pachpute <prjpachpute@gmail.com>
2025-04-25 10:09:45 +0530

    again added firebase acc file

--- app/src/main/java/com/example/bookxpertapplication/firebase/GoogleAccessTokenProvider.kt
+++ app/src/main/java/com/example/bookxpertapplication/firebase/GoogleAccessTokenProvider.kt
@@ -6,11 +6,16 @@ import com.google.auth.oauth2.GoogleCredentials
 
 object GoogleAccessTokenProvider {
     fun getAccessToken(context: Context): String? {
-        val stream = context.assets.open("bookxpertapplication-service-account.json")
-        val credentials = GoogleCredentials.fromStream(stream)
-            .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"))
+        return try {
+            val stream = context.assets.open("services-account.json")
+            val credentials = GoogleCredentials.fromStream(stream)
+                .createScoped(listOf("https://www.googleapis.com/auth/firebase.messaging"))
 
-        credentials.refreshIfExpired()
-        return credentials.accessToken.tokenValue
+            credentials.refreshIfExpired()
+            credentials.accessToken.tokenValue
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 }
